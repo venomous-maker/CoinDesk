e777f887c2552f9c4a8a7510e2962fdfc1e12e4c
venomous-maker <morganokumu8+1@gmail.com>
2024-12-30 12:21:02 +0300

    Update widget

--- resources/js/Pages/CoinDesk/Trade/CoinDeskTrading.vue
+++ resources/js/Pages/CoinDesk/Trade/CoinDeskTrading.vue
@@ -8,11 +8,40 @@
         <br />
         <br />
 
+        <Chart
+            :options="{
+        symbol: 'BITSTAMP:BTCUSD',
+        timezone: 'Pacific/Chatham',
+        theme: 'dark',
+        style: 1,
+        locale: 'en',
+        backgroundColor: 'rgba(0, 0, 0, 1)',
+        withdateranges: true,
+        range: 'ALL',
+        hide_side_toolbar: false,
+        allow_symbol_change: true,
+        details: true,
+        calendar: true,
+        show_popup_button: true,
+        popup_width: '1000',
+        popup_height: '650',
+        support_host: 'https://www.tradingview.com'
+    }"
+        />
 
-        <Chart />
-        <CryptoMarket />
-<!--        <Snaps/>-->
-        <Screener/>
+        <CryptoMarket :options="{ theme: theme }" />
+        <!--        <Snaps/>-->
+        <Screener
+            :options="{
+
+                defaultColumn: 'overview',
+                defaultScreen: 'general',
+                market: 'bitcoin',
+                showToolbar: true,
+                colorTheme: 'dark',
+                locale: 'en',
+            }"
+        />
         <br />
     </div>
 </template>
@@ -24,6 +53,12 @@ import { defineComponent } from 'vue';
 import { Chart, CryptoMarket, Screener, Snaps } from 'vue-tradingview-widgets';
 
 export default defineComponent({
+    props: {
+        theme: {
+            type: String,
+            default: 'dark',
+        },
+    },
     name: 'App',
     components: {
         Chart,
