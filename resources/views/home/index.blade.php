de12fa8a79db274a9698d1068c111c978cbba28d
venomous-maker <morganokumu8+1@gmail.com>
2025-01-02 11:56:18 +0300

    Removed Unnecessary elements

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,16 +5,16 @@
 {{--        <coin-desk-marquee-scroll></coin-desk-marquee-scroll>--}}
         <coin-desk-ticker-tape-marquee-scroll></coin-desk-ticker-tape-marquee-scroll>
         <coin-desk-our-value></coin-desk-our-value>
-        <coin-desk-about-company></coin-desk-about-company>
+{{--        <coin-desk-about-company></coin-desk-about-company>--}}
         <coin-desk-why-choose-us></coin-desk-why-choose-us>
         <coin-desk-company-details></coin-desk-company-details>
         <coin-desk-how-it-work></coin-desk-how-it-work>
         <coin-desk-currency-convertor></coin-desk-currency-convertor>
-        <coin-desk-our-team-member></coin-desk-our-team-member>
+{{--        <coin-desk-our-team-member></coin-desk-our-team-member>--}}
         <coin-desk-any-query></coin-desk-any-query>
-        <coin-desk-latest-blog></coin-desk-latest-blog>
-        <coin-desk-contact-us></coin-desk-contact-us>
-        <coin-desk-what-our-clients-say></coin-desk-what-our-clients-say>
-        <coin-desk-our-clients></coin-desk-our-clients>
+{{--        <coin-desk-latest-blog></coin-desk-latest-blog>--}}
+{{--        <coin-desk-contact-us></coin-desk-contact-us>--}}
+{{--        <coin-desk-what-our-clients-say></coin-desk-what-our-clients-say>--}}
+{{--        <coin-desk-our-clients></coin-desk-our-clients>--}}
     </div>
 @endsection
