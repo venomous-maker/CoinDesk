809dd9494feed62de6ece790d35c921046f197c6
venomous-maker <morganokumu8+1@gmail.com>
2025-01-03 19:51:40 +0300

    Fixed Our Value lorem ipsum

--- resources/js/Components/CoinDesk/Sections/CoinDeskOurValue.vue
+++ resources/js/Components/CoinDesk/Sections/CoinDeskOurValue.vue
@@ -1,25 +1,38 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <div class="section-full site-bg-primary">
         <div class="container">
-            <div class="section-content ">
+            <div class="section-content">
                 <!-- COLL-TO ACTION START -->
                 <div class="wt-subscribe-box">
                     <div class="container">
                         <div class="row call-to-action-wrap">
                             <div class="col-lg-9 col-md-12">
-                                <div class="call-to-action-left ">
-                                    <h4 class="text-uppercase m-b10 font-weight-600">Invest in Cryptocurrency Bitcoin Mining & Easy Way to Trade Bitcoin.</h4>
-                                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore edolore magna aliquyam erat.</p>
+                                <div class="call-to-action-left">
+                                    <h4
+                                        class="text-uppercase m-b10 font-weight-600"
+                                    >
+                                        Invest in Cryptocurrency Bitcoin Mining
+                                        & Easy Way to Trade Bitcoin.
+                                    </h4>
+                                    <p>
+                                        Explore the world of cryptocurrency with
+                                        Bitcoin mining and seamless trading
+                                        options. Whether you're mining or
+                                        trading, Bitcoin offers a secure and
+                                        efficient way to grow your wealth in the
+                                        digital era.
+                                    </p>
                                 </div>
                             </div>
 
                             <div class="col-lg-3 col-md-12">
                                 <div class="call-to-action-right">
-                                    <a href="contact-1.html" class="site-button-secondry text-uppercase font-weight-600">
+                                    <a
+                                        href="contact-1.html"
+                                        class="site-button-secondry text-uppercase font-weight-600"
+                                    >
                                         Read More
                                     </a>
                                 </div>
@@ -32,6 +45,4 @@
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
