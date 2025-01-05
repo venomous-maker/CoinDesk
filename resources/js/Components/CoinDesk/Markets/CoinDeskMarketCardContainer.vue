c5299ac6cbe66c5a35c44fe67426edf986f4bd1d
venomous-maker <morganokumu8+1@gmail.com>
2025-01-06 00:27:09 +0300

    Fixed market

---
+++ resources/js/Components/CoinDesk/Markets/CoinDeskMarketCardContainer.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="d-flex justify-space-between">
+        <v-card
+            v-for="(item, index) in cryptoData"
+            :key="index"
+            class="mx-2"
+            flat
+            tile
+            outlined
+        >
+            <v-card-title class="d-flex justify-space-between">
+                <div>
+                    <div>{{ item.pair }}</div>
+                    <div :class="item.change > 0 ? 'text-success' : 'text-error'">
+                        {{ item.change }}%
+                    </div>
+                </div>
+                <v-spacer></v-spacer>
+                <div>
+                    <v-icon>mdi-chart-line</v-icon>
+                </div>
+            </v-card-title>
+            <v-card-text>
+                <div class="text-h5">{{ item.price }}</div>
+                <div class="text-caption">≈ $ {{ item.usdEquivalent }}</div>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cryptoData: [
+                {
+                    pair: "BTC/USDT",
+                    change: 0.15,
+                    price: "98363.64",
+                    usdEquivalent: "98363.64",
+                },
+                {
+                    pair: "ETH/USDT",
+                    change: -0.51,
+                    price: "3638.69",
+                    usdEquivalent: "3638.69",
+                },
+                {
+                    pair: "XRP/USDT",
+                    change: -0.75,
+                    price: "2.4011",
+                    usdEquivalent: "2.4",
+                },
+                {
+                    pair: "TRX/USDT",
+                    change: -2.60,
+                    price: "0.2627",
+                    usdEquivalent: "0.26",
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style scoped>
+.text-success {
+    color: green;
+}
+.text-error {
+    color: red;
+}
+</style>
--- resources/js/Components/CoinDesk/Markets/CoinDeskMarketIndex.vue
+++ resources/js/Components/CoinDesk/Markets/CoinDeskMarketIndex.vue
@@ -2,19 +2,20 @@
 import { defineComponent } from 'vue';
 // Lets ignore build check here to avoid errors
 // @ts-ignore
+import CoinDeskMarketCardContainer from '@/Components/CoinDesk/Markets/CoinDeskMarketCardContainer.vue';
 import CoinDeskMainWrapper from '@/Components/CoinDesk/Wrappers/CoinDeskMainWrapper.vue';
 import {
     Chart,
     CryptoMarket,
+    MarketData,
+    MarketOverview,
     MiniChart,
     Screener,
+    SingleTicker,
     Snaps,
     StockMarket,
-    TechnicalAnalysis,
-    SingleTicker,
     SymbolOverview,
-    MarketOverview,
-    MarketData
+    TechnicalAnalysis,
 } from 'vue-tradingview-widgets';
 import { route } from 'ziggy-js';
 
@@ -27,6 +28,7 @@ export default defineComponent({
     },
     name: 'App',
     components: {
+        CoinDeskMarketCardContainer,
         CoinDeskMainWrapper,
         Chart,
         CryptoMarket,
@@ -38,29 +40,42 @@ export default defineComponent({
         SingleTicker,
         SymbolOverview,
         MarketOverview,
-        MarketData
-
+        MarketData,
     },
 });
 </script>
 
 <template>
     <coin-desk-main-wrapper :with-space="false">
+        <coin-desk-market-card-container />
         <div class="widget-container">
-            <MiniChart :options="{ colorTheme: 'dark', height: 420, symbol: 'FX:EURUSD', largeChartUrl: route('trade.trading'), }" />
-            <TechnicalAnalysis :options="{ colorTheme: 'dark', height: 420, symbol: 'FX:EURUSD', largeChartUrl: route('trade.trading'), }" />
-            <SingleTicker :options="{ colorTheme: 'dark',height: 420, symbol: 'FX:EURUSD', largeChartUrl: route('trade.trading'), }" />
-            <SymbolOverview :options="{ colorTheme: 'dark', width: '80%', height: 420, largeChartUrl: route('trade.trading'),}" />
+            <MiniChart
+                :options="{
+                    colorTheme: 'dark',
+                    symbol: 'FX:EURUSD',
+                    largeChartUrl: route('trade.trading'),
+                }"
+            />
+            <!--            <TechnicalAnalysis :options="{ colorTheme: 'dark', height: 420, symbol: 'FX:EURUSD', largeChartUrl: route('trade.trading'), }" />-->
+<!--            <SingleTicker-->
+<!--                :options="{-->
+<!--                    colorTheme: 'dark',-->
+<!--                    height: 420,-->
+<!--                    symbol: 'FX:EURUSD',-->
+<!--                    largeChartUrl: route('trade.trading'),-->
+<!--                }"-->
+<!--            />-->
+            <!--            <SymbolOverview :options="{ colorTheme: 'dark', width: '80%', height: 420, largeChartUrl: route('trade.trading'),}" />-->
         </div>
-<!--        <CryptoMarket-->
-<!--            :options="{-->
-<!--                colorTheme: 'dark',-->
-<!--                width: '100%',-->
-<!--                height: 610,-->
-<!--                market: 'bitcoin',-->
-<!--                largeChartUrl: route('trade.trading'),-->
-<!--            }"-->
-<!--        />-->
+        <!--        <CryptoMarket-->
+        <!--            :options="{-->
+        <!--                colorTheme: 'dark',-->
+        <!--                width: '100%',-->
+        <!--                height: 610,-->
+        <!--                market: 'bitcoin',-->
+        <!--                largeChartUrl: route('trade.trading'),-->
+        <!--            }"-->
+        <!--        />-->
         <Screener
             :options="{
                 width: '100%',
